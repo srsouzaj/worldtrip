3ffc513571bdf84d7a43762874d97bc85b82fba0
Jorge de Souza <jorgejrdj1@gmail.com>
2022-01-29 23:11:47 -0300

    Finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
-import { Flex, Grid, GridItem, Icon, Image } from "@chakra-ui/react";
-import { useRouter } from "next/dist/client/router";
-import { RiArrowLeftSLine } from 'react-icons/ri';
-import Link from 'next/link';
+import { Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { RiArrowLeftLine } from 'react-icons/ri'
+import Link from "next/link";
+
 
 export default function Header() {
-    const { asPath } = useRouter()
+    const { asPath } = useRouter();
     const notHomePage = asPath !== '/'
 
     return (
-        <Flex bg="white" w="100%" as="header" mx="auto" px="1rem" h={["50px", "100px"]} align="center" justify="center">
+        <Flex bg="white"
+            w="100%"
+            as="header"
+            mx="auto"
+            px="1rem"
+            h={["50px",
+                "100px"]}
+            align="center"
+            justify="center"
+        >
+
             <Grid
                 h="100%"
                 mx="auto"
@@ -16,24 +27,24 @@ export default function Header() {
                 maxW="1160px"
                 alignItems="center"
                 templateColumns="repeat(3, 1fr)"
-                justifyContent="center"
-                alignSelf="start"
-            >
+                justifyContent="center">
+
                 {notHomePage && (
                     <Link href="/">
                         <a>
-                            <Icon as={RiArrowLeftSLine} fontSize={[20, 40]} justifySelf="start" />
+                            <Icon as={RiArrowLeftLine} fontSize={[20, 40]} justifySelf="start" />
                         </a>
                     </Link>
                 )}
                 <Image
                     w={["81px", "184px"]}
                     src="/logo.svg"
-                    alt="Um avição voando sobre o nome da marca world trip"
+                    alt="Um avião voando sobre o nome da marca world trip"
                     justifySelf="center"
                     gridColumn="2"
                 />
             </Grid>
         </Flex>
+
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Caracteristicas from '../components/Caracteristicas'
+import Separador from '../components/Separador'
+import Slider from '../components/Slider'
 
+interface HomeProps {
+  continents:{
+    slug: string;
+    title: string;
+    summary: string;
+    image: string;
+  }[]
+}
 
-export default function Home() {
+export default function Home({continents}: HomeProps) {
   return (
     <Flex direction="column">
       <Header />
 
       <Banner />
+
+      <Caracteristicas />
+      <Separador />
+
+      <Heading
+        textAlign='center'
+        fontWeight='500'
+        mb={['5', '14']}
+        fontSize={['lg', '3xl', '4xl']}
+      >
+        Vamos nessa? <br /> Então escolha seu continente
+      </Heading>
+
+      <Slider />
 
     </Flex>
   )
